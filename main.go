da3a78183af1e993b75409d944b8e878936963dc
= <adnanturan2000@gmail.com>
2024-07-25 22:26:27 +0100

    Updated server to use chi as a router

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func chatRoomLogin(w http.ResponseWriter, r *http.Request) {
@@ -12,6 +15,8 @@ func chatRoomLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", chatRoomLogin)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", chatRoomLogin)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
