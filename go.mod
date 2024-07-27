925aebdbb94ef052040d10cecdc9d5f668de61ee
= <adnanturan2000@gmail.com>
2024-07-27 12:05:35 +0100

    Added feature to add user to active server users; Next need to navigate user to dashboard and add event to handle when user disconnects

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module chat-system
 
 go 1.22.5
 
-require github.com/go-chi/chi/v5 v5.1.0 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,22 +1,85 @@
 package main
 
 import (
-	"html/template"
+	"encoding/json"
+	"fmt"
 	"log"
+
+	"html/template"
 	"net/http"
+
+	// "sync"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/gorilla/websocket"
 )
 
-func chatRoomLogin(w http.ResponseWriter, r *http.Request) {
-	template := template.Must(template.ParseFiles("./pages/chatRoomLogin.html"))
-	template.Execute(w, nil)
+type Client struct {
+	Username   string
+	Connection *websocket.Conn
+}
+
+var server_active_users = make(map[string]*Client)
+
+var upgrader = websocket.Upgrader{
+	WriteBufferSize: 1024,
+	ReadBufferSize:  1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+// Auxiliary Functions
+func sendJsonResponse(w http.ResponseWriter, data any) {
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
+
+// End Points
+
+func userLogin(w http.ResponseWriter, r *http.Request) {
+	pageTemplate := template.Must(template.ParseFiles("./pages/chatRoomLogin.html"))
+	pageTemplate.Execute(w, nil)
+}
+
+func addUser(w http.ResponseWriter, r *http.Request) {
+	// parse form
+	err := r.ParseForm()
+	if err != nil {
+		w.Write([]byte("Error parsing form"))
+	}
+
+	// get username
+	username := r.PostFormValue("username")
+
+	_, user_already_exists := server_active_users[username]
+	if user_already_exists {
+		w.Write([]byte(fmt.Sprintf("User %s already exists", username)))
+	} else {
+		log.Println("Adding new user")
+		connection, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			w.Write([]byte("Error was not able to establish websocket connection"))
+		}
+		server_active_users[username] = &Client{
+			Username:   username,
+			Connection: connection,
+		}
+	}
+
 }
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", chatRoomLogin)
-	log.Fatal(http.ListenAndServe(":8000", r))
+
+	// define chi router
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	// define paths
+	router.Get("/", userLogin)
+	router.Post("/add_user", addUser)
+
+	// start server
+	http.ListenAndServe(":8080", router)
+
 }
